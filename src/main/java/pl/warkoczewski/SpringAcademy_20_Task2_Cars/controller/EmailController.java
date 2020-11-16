03af4559d485f16b46ded16b2eb938c2166728ad
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-11-16 19:44:36 +0100

    SimpleEmailMessage

--- src/main/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/controller/EmailController.java
+++ src/main/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/controller/EmailController.java
@@ -1,9 +1,12 @@
 package pl.warkoczewski.SpringAcademy_20_Task2_Cars.controller;
 
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("email")
@@ -14,7 +17,16 @@ public class EmailController {
         return "email/emailForm";
     }
     @PostMapping
-    public String processEmailForm(){
+    public String processEmailForm(HttpServletRequest request){
+        String fullName = request.getParameter("fullName");
+        String email = request.getParameter("email");
+        String subject = request.getParameter("subject");
+        String content = request.getParameter("content");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("andresik82@gmail.com");
+        message.setTo(email);
+        message.setSubject(subject);
+        message.setText(content);
 
         return "email/emailForm";
     }
