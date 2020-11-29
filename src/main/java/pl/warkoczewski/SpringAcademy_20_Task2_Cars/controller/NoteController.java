62c3896de56c567307e4d381aadc6f2dc9c389f3
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-11-29 09:52:09 +0100

    NoteController

--- src/main/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/controller/NoteController.java
+++ src/main/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/controller/NoteController.java
@@ -1,11 +1,27 @@
 package pl.warkoczewski.SpringAcademy_20_Task2_Cars.controller;
 
 import org.springframework.stereotype.Controller;
-
-
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.warkoczewski.SpringAcademy_20_Task2_Cars.dto.NoteDTO;
 
 @Controller
+@RequestMapping("/notepad")
 public class NoteController {
+    @GetMapping
+    public String displayHomePage(){
+        return "notepad/home";
+    }
+    @GetMapping("/add")
+    public String displayAddNotePage(Model model){
+        model.addAttribute("noteDTO", new NoteDTO());
+        return "notepad/add";
+    }
+    @PostMapping("/add")
+    public String processAddingNoteForm(){
+        return "notepad/add";
+    }
 
 }
--- src/main/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/dto/NoteDTO.java
+++ src/main/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/dto/NoteDTO.java
@@ -1,5 +1,15 @@
 package pl.warkoczewski.SpringAcademy_20_Task2_Cars.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NoteDTO {
+
+    private String text;
+    private String username;
 
 }
