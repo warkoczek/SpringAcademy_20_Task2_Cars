16ffa65dae0705fc8a2fa2d82d815a0aa391ccf8
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-12-01 08:26:57 +0100

    Topic enum in Thymeleaf refactor

--- src/main/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/model/Topic.java
+++ src/main/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/model/Topic.java
@@ -1,5 +1,14 @@
 package pl.warkoczewski.SpringAcademy_20_Task2_Cars.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum  Topic {
-    GENERAL, EVENT, REMINDER, HOROSCOPE, PAYMENT, COST, DUTY, DRAFT
+    @JsonProperty("GENERAL")GENERAL,
+    @JsonProperty("EVENT")EVENT,
+    @JsonProperty("REMINDER")REMINDER,
+    @JsonProperty("HOROSCOPE")HOROSCOPE,
+    @JsonProperty("PAYMENT")PAYMENT,
+    @JsonProperty("COST")COST,
+    @JsonProperty("DUTY")DUTY,
+    @JsonProperty("DRAFT")DRAFT
 }
