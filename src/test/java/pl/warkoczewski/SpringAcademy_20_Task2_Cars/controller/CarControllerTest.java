8af0005bf9f6032a6877e8fa4f09ac968b4643d9
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-12-14 18:39:27 +0100

    erased libraries

--- src/test/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/controller/CarControllerTest.java
+++ src/test/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/controller/CarControllerTest.java
@@ -7,12 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
