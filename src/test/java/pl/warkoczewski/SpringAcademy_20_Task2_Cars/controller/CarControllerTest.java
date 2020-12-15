577221ac11659540f30b4811b228cdc6f3725973
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-12-15 21:32:36 +0100

    @Test void getCars_Should_Return_List_Size_3() passes

--- src/test/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/controller/CarControllerTest.java
+++ src/test/java/pl/warkoczewski/SpringAcademy_20_Task2_Cars/controller/CarControllerTest.java
@@ -2,19 +2,37 @@ package pl.warkoczewski.SpringAcademy_20_Task2_Cars.controller;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class CarControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void getCars() {
+    void getCars_Should_Return_List_Size_3() throws Exception {
+        //given
+        mockMvc.perform(get("/cars").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)));
+        //when
+        //then
+
     }
 
     @Test
